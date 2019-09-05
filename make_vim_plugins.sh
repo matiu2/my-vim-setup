1b2c3d392f55bbc3713748e10a3487ed4ac8708d
Matthew Sherborne <matt.sherborne@rackspace.com>
2019-09-05 10:27:12 +1000

    Initial commit

---
+++ make_vim_plugins.sh
@@ -0,0 +1,18 @@
+mkdir -p ~/.vim/pack/plugins/start/
+cd ~/.vim/pack/plugins/start/
+git clone --shallow https://github.com/prabirshrestha/async.vim.git
+git clone --shallow https://github.com/prabirshrestha/asyncomplete-lsp.vim.git
+git clone --shallow https://github.com/prabirshrestha/asyncomplete.vim.git
+git clone --shallow https://github.com/maralla/completor.vim.git
+git clone --shallow https://github.com/morhetz/gruvbox.git
+git clone --shallow https://github.com/rust-lang/rust.vim
+git clone --shallow https://github.com/vim-syntastic/syntastic.git
+git clone --shallow https://github.com/majutsushi/tagbar.git
+git clone --shallow https://github.com/SirVer/ultisnips.git
+git clone --shallow https://github.com/tpope/vim-fugitive.git
+git clone --shallow https://github.com/prabirshrestha/vim-lsp.git
+git clone --shallow https://github.com/xolox/vim-misc.git
+git clone --shallow https://github.com/xolox/vim-notes.git
+git clone --shallow https://github.com/honza/vim-snippets.git
+git clone --shallow https://github.com/tpope/vim-surround.git
+git clone --shallow https://github.com/mattn/webapi-vim.git
